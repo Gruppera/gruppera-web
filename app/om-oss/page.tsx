659160809bc3053eb846af9e4adb68d074e03b5f
Olle Reuterhall <olle@gruppera.se>
2026-01-30 16:26:41 +0100

    changed photos for the blog mock

--- app/om-oss/page.tsx
+++ app/om-oss/page.tsx
@@ -7,28 +7,8 @@ export default function OmOssPage() {
   return (
     <Container size="lg" py={{ base: "lg", sm: "xl" }}>
       <Stack gap="sm">
-        <Title order={1} fz={{ base: 36, md: 52 }}>
-          Om oss
-        </Title>
         <FeaturesGrid>
         </FeaturesGrid>
-        <Container my="md">
-          <Title>Kolla hur kul vi har det!</Title>
-      <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="md">
-        {/* <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} /> */}
-        <Grid gutter="md">
-          <GridCol>
-            <Image src={"/photos/kul/20260114_191113.jpg"}></Image>
-          </GridCol>
-          <GridCol span={6}>
-            <Image src={"/photos/kul/20260114_191210.jpg"}></Image>
-          </GridCol>
-          <GridCol span={6}>
-            <Image src={"/photos/kul/20251112_085828.JPG"}></Image>
-          </GridCol>
-        </Grid>
-      </SimpleGrid>
-    </Container>
       </Stack>
     </Container>
   );
